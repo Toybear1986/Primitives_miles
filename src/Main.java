766f40558d17848624326510a877ed7730d8ae55
Toybear1986 <vaskorobogatov@yandex.ru>
2022-07-15 09:19:34 +0300

    Camelcase applied

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         int amount = 10_000;
-        int for_one_mile = 20;
+        int forOneMile = 20;
 
-        int bonus = amount / for_one_mile;
+        int bonus = amount / forOneMile;
 
         System.out.println("Вы получите " + bonus + " миль за этот заказ");
     }
